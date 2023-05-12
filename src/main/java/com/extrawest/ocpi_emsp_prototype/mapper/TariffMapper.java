76044d17d9cb09bd58517103ea4184fb08225fe5
vladyslav.tertyshnyi <vladyslav.tertyshnyi@extrawest.com>
2023-05-12 13:53:19 +0300

    added TariffMapper.java

---
+++ src/main/java/com/extrawest/ocpi_emsp_prototype/mapper/TariffMapper.java
@@ -0,0 +1,16 @@
+package com.extrawest.ocpi_emsp_prototype.mapper;
+
+import com.extrawest.ocpi_emsp_prototype.dto.TariffRequestDTO;
+import com.extrawest.ocpi_emsp_prototype.dto.TariffResponseDTO;
+import com.extrawest.ocpi_emsp_prototype.model.Tariff;
+import org.mapstruct.Mapper;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface TariffMapper {
+    Tariff toTariffEntity(TariffRequestDTO tariffRequestDTO);
+    TariffResponseDTO toTariffResponseDTO(Tariff tariff);
+    List<TariffResponseDTO> toListTariffResponseDTO(List<Tariff> tariffs);
+    List<Tariff> toListTariff(List<TariffRequestDTO> tariffs);
+}
