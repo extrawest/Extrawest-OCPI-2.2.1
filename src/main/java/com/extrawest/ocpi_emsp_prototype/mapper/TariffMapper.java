cc0de9bbcc911935924d3fd6ef87d48a0e43c8aa
vladyslav.tertyshnyi <vladyslav.tertyshnyi@extrawest.com>
2023-05-26 16:10:48 +0300

    added TariffMapper interface for Tariff entity mapping

--- src/main/java/com/extrawest/ocpi_emsp_prototype/mapper/TariffMapper.java
+++ src/main/java/com/extrawest/ocpi_emsp_prototype/mapper/TariffMapper.java
@@ -1,7 +1,7 @@
 package com.extrawest.ocpi_emsp_prototype.mapper;
 
-import com.extrawest.ocpi_emsp_prototype.dto.TariffRequestDTO;
-import com.extrawest.ocpi_emsp_prototype.dto.TariffResponseDTO;
+import com.extrawest.ocpi_emsp_prototype.model.dto.request.TariffRequestDTO;
+import com.extrawest.ocpi_emsp_prototype.model.dto.response.TariffResponseDTO;
 import com.extrawest.ocpi_emsp_prototype.model.Tariff;
 import org.mapstruct.Mapper;
 
