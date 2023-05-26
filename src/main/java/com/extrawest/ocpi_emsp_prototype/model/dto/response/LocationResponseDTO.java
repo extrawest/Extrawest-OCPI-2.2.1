a20d7cbcbd039b413eb77df9d34320c25086ad6b
vladyslav.tertyshnyi <vladyslav.tertyshnyi@extrawest.com>
2023-05-26 16:08:34 +0300

    created DTO`s for Location model

---
+++ src/main/java/com/extrawest/ocpi_emsp_prototype/model/dto/response/LocationResponseDTO.java
@@ -0,0 +1,43 @@
+package com.extrawest.ocpi_emsp_prototype.model.dto.response;
+
+import com.extrawest.ocpi_emsp_prototype.model.dataTypes.*;
+import com.extrawest.ocpi_emsp_prototype.model.enums.Facility;
+import com.extrawest.ocpi_emsp_prototype.model.enums.ParkingType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class LocationResponseDTO {
+    private String countryCode;
+    private String partyId;
+    private String id;
+    private Boolean publish;
+    private List<PublishTokenType> publishAllowedTo;
+    private String name;
+    private String address;
+    private String city;
+    private String postalCode;
+    private String state;
+    private String country;
+    private GeoLocation coordinates;
+    private List<AdditionalGeoLocation> relatedLocations;
+    private ParkingType parkingType;
+    private List<EVSE> evses;
+    private List<DisplayText> directions;
+    private BusinessDetails operator;
+    private BusinessDetails subOperator;
+    private BusinessDetails owner;
+    private List<Facility> facilities;
+    private String timeZone;
+    private Hours openingTimes;
+    private Boolean chargingWhenClosed;
+    private List<Image> images;
+    private EnergyMix energyMix;
+    private LocalDateTime lastUpdated;
+}
