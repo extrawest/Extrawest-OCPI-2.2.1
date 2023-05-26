cec3395b1b6286f66e669ebcdf31eeed1bb4c50b
vladyslav.tertyshnyi <vladyslav.tertyshnyi@extrawest.com>
2023-05-26 16:10:22 +0300

    added new parameters, documentation and changed mapping for Tariff entity controllers

--- src/main/java/com/extrawest/ocpi_emsp_prototype/controller/cpo/CPOTariffController.java
+++ src/main/java/com/extrawest/ocpi_emsp_prototype/controller/cpo/CPOTariffController.java
@@ -1,19 +1,32 @@
 package com.extrawest.ocpi_emsp_prototype.controller.cpo;
 
-import com.extrawest.ocpi_emsp_prototype.dto.TariffResponseDTO;
-import lombok.AllArgsConstructor;
+import com.extrawest.ocpi_emsp_prototype.model.dto.response.TariffResponseDTO;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
 @RequestMapping("/cpo/api/tariffs")
-@AllArgsConstructor
 public abstract class CPOTariffController {
 
-    @GetMapping("/pullModel")
-    public abstract ResponseEntity<List<TariffResponseDTO>> saveTariff();
+    /**
+     * Returns Tariff objects from the CPO, last updated between the {date_from} and {date_to} (paginated)
+     * @param dateFrom Only return Tariffs that have last_updated after or equal to this Date/Time (inclusive).
+     * @param dateTo Only return Tariffs that have last_updated up to this Date/Time, but not including (exclusive).
+     * @param offset The offset of the first object returned. Default is 0.
+     * @param limit Maximum number of objects to GET
+     * @return List of all tariffs.
+     */
+    @GetMapping("/getTariffs")
+    public abstract ResponseEntity<List<TariffResponseDTO>> saveTariff(
+            @RequestParam(value = "date_from", required = false) LocalDateTime dateFrom,
+            @RequestParam(value = "date_to", required = false) LocalDateTime dateTo,
+            @RequestParam(value = "offset", required = false, defaultValue = "0") Integer offset,
+            @RequestParam(value = "limit", required = false) Integer limit
+    );
 }
--- src/main/java/com/extrawest/ocpi_emsp_prototype/controller/emsp/EMSPTariffController.java
+++ src/main/java/com/extrawest/ocpi_emsp_prototype/controller/emsp/EMSPTariffController.java
@@ -1,18 +1,54 @@
 package com.extrawest.ocpi_emsp_prototype.controller.emsp;
 
-import com.extrawest.ocpi_emsp_prototype.dto.TariffRequestDTO;
+import com.extrawest.ocpi_emsp_prototype.model.dto.request.TariffRequestDTO;
+import com.extrawest.ocpi_emsp_prototype.model.dto.response.TariffResponseDTO;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/emsp/api/tariffs")
 public abstract class EMSPTariffController {
 
-    @PutMapping("/pushModel")
-    public abstract ResponseEntity<String> saveTariff(@RequestBody List<TariffRequestDTO> tariffRequestDTOList);
+    /**
+     * Retrieve a Tariff as it is stored in the eMSP’s system.
+     * @param countryCode Country code of the CPO performing the GET request on the eMSP’s system.
+     * @param partyId Party ID (Provider ID) of the CPO performing the GET request on the eMSP’s system.
+     * @param tariff_id Party ID (Provider ID) of the CPO performing the GET request on the eMSP’s system.
+     * @return The requested Tariff object.
+     */
+    @GetMapping("/getTariff")
+    public abstract ResponseEntity<TariffResponseDTO> getTariff(
+            @RequestParam(value = "country_code") String countryCode,
+            @RequestParam(value = "party_id") String partyId,
+            @RequestParam(value = "tariff_id") String tariff_id
+    );
+
+    /**
+     * Push new/updated Tariff object to the eMSP.
+     * @param tariffRequestDTO New or updated Tariff object.
+     * @param countryCode Country code of the CPO performing the PUT request on the eMSP’s system. This SHALL be
+     *                    the same value as the country_code in the Tariff object being pushed.
+     * @param partyId Party ID (Provider ID) of the CPO performing the PUT request on the eMSP’s system. This SHALL be the same value as the party_id in the Tariff object being pushed.
+     * @param tariff_id Tariff.id of the Tariff object to create or replace.
+     */
+    @PutMapping("/putTariff")
+    public abstract void saveTariff(
+            @RequestBody TariffRequestDTO tariffRequestDTO,
+            @RequestParam(value = "country_code") String countryCode,
+            @RequestParam(value = "party_id") String partyId,
+            @RequestParam(value = "tariff_id") String tariff_id
+    );
+
+    /**
+     * Delete a Tariff object which is not used any more and will not be used in the future.
+     * @param countryCode Country code of the CPO performing the PUT request on the eMSP’s system.
+     * @param partyId Party ID (Provider ID) of the CPO performing the PUT request on the eMSP’s system.
+     * @param tariff_id Tariff.id of the Tariff object to delete.
+     */
+    @DeleteMapping("/deleteTariff")
+    public abstract void deleteTariff(
+            @RequestParam(value = "country_code") String countryCode,
+            @RequestParam(value = "party_id") String partyId,
+            @RequestParam(value = "tariff_id") String tariff_id
+    );
 }
