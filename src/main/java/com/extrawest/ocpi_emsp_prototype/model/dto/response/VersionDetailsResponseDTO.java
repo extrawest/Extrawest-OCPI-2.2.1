a549672fb65afa5de716584bd71c1826086aa673
vladyslav.tertyshnyi <vladyslav.tertyshnyi@extrawest.com>
2023-06-05 13:11:39 +0300

    added @AllArgsConstructor @NoArgsConstructor annotation for response DTO`s

--- src/main/java/com/extrawest/ocpi_emsp_prototype/model/dto/response/VersionDetailsResponseDTO.java
+++ src/main/java/com/extrawest/ocpi_emsp_prototype/model/dto/response/VersionDetailsResponseDTO.java
@@ -2,11 +2,15 @@ package com.extrawest.ocpi_emsp_prototype.model.dto.response;
 
 import com.extrawest.ocpi_emsp_prototype.model.enums.VersionNumber;
 import com.extrawest.ocpi_emsp_prototype.model.vo.Endpoint;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class VersionDetailsResponseDTO {
     private VersionNumber version;
     private List<Endpoint> endpoints;
--- src/main/java/com/extrawest/ocpi_emsp_prototype/model/dto/response/VersionResponseDTO.java
+++ src/main/java/com/extrawest/ocpi_emsp_prototype/model/dto/response/VersionResponseDTO.java
@@ -1,9 +1,13 @@
 package com.extrawest.ocpi_emsp_prototype.model.dto.response;
 
 import com.extrawest.ocpi_emsp_prototype.model.enums.VersionNumber;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class VersionResponseDTO {
     public VersionNumber version;
     public String url;
