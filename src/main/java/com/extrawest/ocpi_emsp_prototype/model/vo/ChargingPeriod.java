924a3454e1f9b4c008f74a8183e6083040d9c808
vladyslav.tertyshnyi <vladyslav.tertyshnyi@extrawest.com>
2023-06-07 12:52:43 +0300

    added new volume object classes with validation for CDR module

---
+++ src/main/java/com/extrawest/ocpi_emsp_prototype/model/vo/ChargingPeriod.java
@@ -0,0 +1,72 @@
+package com.extrawest.ocpi_emsp_prototype.model.vo;
+
+import com.extrawest.ocpi_emsp_prototype.validation.ListOfAtLeastOneObjects;
+import com.extrawest.ocpi_emsp_prototype.validation.RequiredValidator;
+import com.extrawest.ocpi_emsp_prototype.validation.Validatable;
+import com.extrawest.ocpi_emsp_prototype.validation.Validator;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+/**
+ * A Charging Period consists of a start timestamp and a list of possible values that influence this period,
+ * for example: amount of energy charged this period, maximum current during this period etc.
+ */
+@Getter
+@ToString
+@EqualsAndHashCode(callSuper = false)
+@NoArgsConstructor
+public class ChargingPeriod implements Validatable {
+
+    @JsonIgnore
+    private final transient Validator requiredValidator = new RequiredValidator();
+
+    @JsonIgnore
+    private final transient Validator dimensionsValidator = new ListOfAtLeastOneObjects();
+
+    /**
+     * Start timestamp of the charging period. A period ends when the next period starts.
+     * The last period ends when the session ends.
+     */
+    @JsonProperty("start_date_time")
+    private LocalDateTime startDateTime;
+
+    /**
+     * List of relevant values for this charging period.
+     */
+    @JsonProperty("dimensions")
+    private List<CdrDimension> dimensions;
+
+    /**
+     * Unique identifier of the Tariff that is relevant for this Charging Period. If not provided,
+     * no Tariff is relevant during this period.
+     */
+    @JsonProperty("tariff_id")
+    private String tariffId;
+
+    public void setStartDateTime(LocalDateTime startDateTime) {
+        requiredValidator.validate(startDateTime);
+        this.startDateTime = startDateTime;
+    }
+
+    public void setDimensions(List<CdrDimension> dimensions) {
+        dimensionsValidator.validate(dimensions);
+        this.dimensions = dimensions;
+    }
+
+    public void setTariffId(String tariffId) {
+        this.tariffId = tariffId;
+    }
+
+    @Override
+    public boolean validate() {
+        return requiredValidator.safeValidate(startDateTime)
+                && dimensionsValidator.safeValidate(dimensions);
+    }
+}
