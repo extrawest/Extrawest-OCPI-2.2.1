fe3ecf91853439fbbd0511986cdbd385d2b878ec
vladyslav.tertyshnyi <vladyslav.tertyshnyi@extrawest.com>
2023-06-07 12:53:38 +0300

    added cpo/emsp controllers for CDR module

---
+++ src/main/java/com/extrawest/ocpi_emsp_prototype/controller/emsp/EMSPCdrController.java
@@ -0,0 +1,26 @@
+package com.extrawest.ocpi_emsp_prototype.controller.emsp;
+
+import com.extrawest.ocpi_emsp_prototype.model.dto.CdrDTO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/emsp/api/cdr")
+public abstract class EMSPCdrController {
+
+    /**
+     * Fetch CDRs from the receivers system.
+     * @param id - id of CDR
+     * @return The endpoint returns the requested CDR, if it exists.
+     */
+    @GetMapping("/{id}")
+    public abstract ResponseEntity<CdrDTO> getCdr(
+            @PathVariable(value = "id") String id
+    );
+
+    @PostMapping
+    public abstract ResponseEntity<String> postCdr(
+            @RequestBody CdrDTO cdrDTO
+    );
+
+}
