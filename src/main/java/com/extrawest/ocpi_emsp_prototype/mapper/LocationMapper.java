9f33be81a7936b47bf6f00d0201564c32c1cf428
vladyslav.tertyshnyi <vladyslav.tertyshnyi@extrawest.com>
2023-06-13 20:34:31 +0300

    location\tariff mappers refactoring using new class names

--- src/main/java/com/extrawest/ocpi_emsp_prototype/mapper/LocationMapper.java
+++ src/main/java/com/extrawest/ocpi_emsp_prototype/mapper/LocationMapper.java
@@ -1,16 +1,15 @@
 package com.extrawest.ocpi_emsp_prototype.mapper;
 
-import com.extrawest.ocpi_emsp_prototype.model.dto.request.LocationRequestDTO;
-import com.extrawest.ocpi_emsp_prototype.model.dto.response.LocationResponseDTO;
 import com.extrawest.ocpi_emsp_prototype.model.Location;
+import com.extrawest.ocpi_emsp_prototype.model.dto.LocationDTO;
 import org.mapstruct.Mapper;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface LocationMapper {
-    Location toLocationEntity(LocationRequestDTO locationRequestDTO);
-    LocationResponseDTO toTariffResponseDTO(Location location);
-    List<LocationResponseDTO> toListLocationResponseDTO(List<Location> locations);
-    List<Location> toListLocation(List<LocationRequestDTO> locations);
+    Location toLocationEntity(LocationDTO locationDTO);
+    LocationDTO toLocationDTO(Location location);
+    List<LocationDTO> toListLocationDTO(List<Location> locations);
+    List<Location> toListLocation(List<LocationDTO> locations);
 }
--- src/main/java/com/extrawest/ocpi_emsp_prototype/mapper/TariffMapper.java
+++ src/main/java/com/extrawest/ocpi_emsp_prototype/mapper/TariffMapper.java
@@ -1,16 +1,15 @@
 package com.extrawest.ocpi_emsp_prototype.mapper;
 
-import com.extrawest.ocpi_emsp_prototype.model.dto.request.TariffRequestDTO;
-import com.extrawest.ocpi_emsp_prototype.model.dto.response.TariffResponseDTO;
 import com.extrawest.ocpi_emsp_prototype.model.Tariff;
+import com.extrawest.ocpi_emsp_prototype.model.dto.TariffDTO;
 import org.mapstruct.Mapper;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface TariffMapper {
-    Tariff toTariffEntity(TariffRequestDTO tariffRequestDTO);
-    TariffResponseDTO toTariffResponseDTO(Tariff tariff);
-    List<TariffResponseDTO> toListTariffResponseDTO(List<Tariff> tariffs);
-    List<Tariff> toListTariff(List<TariffRequestDTO> tariffs);
+    Tariff toTariffEntity(TariffDTO tariffDTO);
+    TariffDTO toTariffDTO(Tariff tariff);
+    List<TariffDTO> toListTariffDTO(List<Tariff> tariffs);
+    List<Tariff> toListTariff(List<TariffDTO> tariffs);
 }
