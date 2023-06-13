e240e44590daae4c805275b6bd2b53049750ba7b
vladyslav.tertyshnyi <vladyslav.tertyshnyi@extrawest.com>
2023-06-13 20:32:49 +0300

    tariff and location dto was refactored into one class for request/response and deleted unused classes

--- src/main/java/com/extrawest/ocpi_emsp_prototype/model/dto/TariffDTO.java
+++ src/main/java/com/extrawest/ocpi_emsp_prototype/model/dto/TariffDTO.java
@@ -1,10 +1,11 @@
-package com.extrawest.ocpi_emsp_prototype.model.dto.request;
+package com.extrawest.ocpi_emsp_prototype.model.dto;
 
 import com.extrawest.ocpi_emsp_prototype.model.vo.DisplayText;
 import com.extrawest.ocpi_emsp_prototype.model.vo.EnergyMix;
 import com.extrawest.ocpi_emsp_prototype.model.vo.Price;
 import com.extrawest.ocpi_emsp_prototype.model.vo.TariffElement;
 import com.extrawest.ocpi_emsp_prototype.model.enums.TariffType;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,19 +16,29 @@ import java.util.List;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class TariffRequestDTO {
+public class TariffDTO {
+    @JsonProperty("country_code")
     private String countryCode;
+    @JsonProperty("party_id")
     private String partyId;
     private String id;
     private String currency;
     private TariffType type;
+    @JsonProperty("tariff_alt_text")
     private List<DisplayText> tariffAltText;
+    @JsonProperty("tariff_alt_url")
     private String tariffAltUrl;
+    @JsonProperty("min_price")
     private Price minPrice;
+    @JsonProperty("max_price")
     private Price maxPrice;
     private List<TariffElement> elements;
+    @JsonProperty("start_date_time")
     private LocalDateTime startDateTime;
+    @JsonProperty("end_date_time")
     private LocalDateTime endDateTime;
+    @JsonProperty("energy_mix")
     private EnergyMix energyMix;
+    @JsonProperty("last_updated")
     private LocalDateTime lastUpdated;
 }
